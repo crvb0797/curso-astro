e37a093c3e258e22065984410228d680c6e0a0ea
Carlos Villatoro <crvb0797@gmail.com>
2023-01-21 21:48:23 -0600

    Astro deploy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https//crvb0797.github.io',
+  base: '/curso-astro',
 });
